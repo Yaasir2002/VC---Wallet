12f55a210c85c37abda529dc5ad4e6d8d43431e0
Yaasir <yaasiraidilfitrah@gmail.com>
2026-06-09 12:12:48 +0700

    benerin issuer trusted

--- src/config/trustedIssuers.ts
+++ src/config/trustedIssuers.ts
@@ -7,10 +7,25 @@ export type TrustedIssuer = {
 
 export const TRUSTED_ISSUERS: TrustedIssuer[] = [
   {
+    id: 'did:web:identitylab.id',
+    name: 'IdentityLab Demo Issuer',
+    allowedCredentialTypes: [
+      'VerifiableCredential',
+      'AcademicCredential',
+      'IdentityCredential',
+      'StudentCredential',
+      'KtpCredential',
+      'KtmCredential',
+      'CustomCredential',
+    ],
+    status: 'active',
+  },
+  {
     id: 'did:web:vc-issuer.yaasir.dev',
     name: 'VC Issuer Yaasir',
     allowedCredentialTypes: [
       'VerifiableCredential',
+      'AcademicCredential',
       'IdentityCredential',
       'StudentCredential',
       'KtpCredential',
